534c2ebf44ed881b291b00397a0d7e52a6461934
seita1996 <taha.seita@gmail.com>
2023-10-22 20:22:00 +0900

    parameter validation

--- myapp/lib/myapp/persistence/relations/books.rb
+++ myapp/lib/myapp/persistence/relations/books.rb
@@ -3,6 +3,9 @@ module Myapp
     module Relations
       class Books < ROM::Relation[:sql]
         schema(:books, infer: true)
+
+        use :pagination
+        per_page 5
       end
     end
   end
