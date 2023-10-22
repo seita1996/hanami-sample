829d10a04bb68192dc3958e34965a6d44ff420f5
seita1996 <taha.seita@gmail.com>
2023-10-22 20:45:40 +0900

    creating a book

--- myapp/config/app.rb
+++ myapp/config/app.rb
@@ -4,5 +4,6 @@ require "hanami"
 
 module Myapp
   class App < Hanami::App
+    config.middleware.use :body_parser, :json
   end
 end
--- myapp/config/routes.rb
+++ myapp/config/routes.rb
@@ -6,5 +6,6 @@ module Myapp
     get "/home/:id", to: "home.show"
     get "/books", to: "books.index"
     get "/books/:id", to: "books.show"
+    post "/books", to: "books.create"
   end
 end
